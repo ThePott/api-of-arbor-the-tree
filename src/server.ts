b22a50d62fc681e772d90d08e9808a7b06b3df0c
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-11-28 17:29:13 +0900

    fix(cicd): change main file in package.json

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import cors, { type CorsOptions } from "cors"
 import checkHealthRouter from "./routers/checkHealthRouter.js"
+import { checkEnvVar } from "./utils/checkEnvVar.js"
 
 const app = express()
 
 const corsOptions: CorsOptions = {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://arbor-the-tree-production.up.railway.app/"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", checkEnvVar(process.env.CLIENT_ORIGIN)],
     methods: ["OPTIONS", "GET", "POST", "PATCH", "PUT", "DELETE"],
 }
 app.use(cors(corsOptions))
