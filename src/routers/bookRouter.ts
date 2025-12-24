590a5671a4bb924abbefa0804465d863b6b4f1cd
Tynahan <nusilite@gmail.com>
2025-12-24 18:08:10 +0900

    feat(book): add search params

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -1,12 +1,30 @@
 import { Router } from "express"
-import { bookArrayDummy } from "../dummy/bookDummy.js"
+import { bookArrayDummy, type Book } from "../dummy/bookDummy.js"
 
 const bookRouter = Router()
 
+type BookActivity = "active" | "inactive" | "total"
+
 bookRouter.get("/", async (req, res) => {
+    const activity = String(req.query.activity) as BookActivity
+
     // NOTE: THIS IS TEMPORARY DUMMY DATA
-    const bookArray = bookArrayDummy
-    res.status(200).json(bookArray)
+    const bookArray: Book[] = bookArrayDummy
+
+    // NOTE: MUST FILTER VIA Prisma
+    const filteredBookArray = bookArray.filter((book) => {
+        switch (activity) {
+            case "active":
+                return book.isActive
+            case "inactive":
+                return !book.isActive
+            case "total":
+                return true
+            default:
+                return true
+        }
+    })
+    res.status(200).json(filteredBookArray)
 })
 
 export default bookRouter
