04742b3c318e1693b32abdce02b9b1a007862d5f
Tynahan <nusilite@gmail.com>
2025-12-31 00:10:58 +0900

    chore(cleanup): remove losing strategy

--- src/db/bookDb.ts
+++ src/db/bookDb.ts
@@ -6,45 +6,10 @@ export const dbCheckIfBookExists = async (title: string): Promise<boolean> => {
     return Boolean(result)
 }
 
-const dbCreateBookWithConcurrency = async ({ title, published_year, data }: BookWritePayload) => {
-    const groupedByTopic = Object.groupBy(data, ({ topic }) => topic)
-    const topicArray = Object.keys(groupedByTopic)
-
-    const bookResult = await prismaClient.book.create({ data: { title, published_year } })
-    const book_id = bookResult.id
-    const topicPromiseArray = topicArray.map(async (topic) => {
-        const topicResult = await prismaClient.topic.create({ data: { title: topic, book_id } })
-        const topic_id = topicResult.id
-        const rowArrayOfTopic = groupedByTopic[topic]
-        if (!rowArrayOfTopic) return
-
-        const groupedByStep = Object.groupBy(rowArrayOfTopic, ({ step }) => step)
-        const stepArray = Object.keys(groupedByStep)
-        const stepPromiseArray = stepArray.map(async (step) => {
-            const stepResult = await prismaClient.step.create({ data: { title: step, topic_id } })
-            const step_id = stepResult.id
-            const rowArrayOfStep = groupedByStep[step]
-            if (!rowArrayOfStep) return
-            await prismaClient.question.createMany({
-                data: rowArrayOfStep.map((row) => ({
-                    name: row.question_name,
-                    page: Number(row.question_page),
-                    solution_page: Number(row.solution_page),
-                    step_id,
-                })),
-            })
-        })
-
-        await Promise.all(stepPromiseArray)
-    })
-
-    await Promise.all(topicPromiseArray)
-}
-
-const dbCreateBookWithRelation = async ({ title, published_year, data }: BookWritePayload) => {
+export const dbCreateBook = async ({ title, published_year, data }: BookWritePayload) => {
     const groupedByTopic = Object.groupBy(data, ({ topic }) => topic)
 
-    const bookResult = await prismaClient.book.create({
+    await prismaClient.book.create({
         data: {
             title,
             published_year,
@@ -69,14 +34,4 @@ const dbCreateBookWithRelation = async ({ title, published_year, data }: BookWri
             },
         },
     })
-
-    return bookResult // TODO: 나중에 어떻게 보낼지 생각하자
-}
-
-export const dbCreateBook = async ({ title, published_year, data }: BookWritePayload) => {
-    // NOTE: 4초 -> 0.5초 미만
-    dbCreateBookWithRelation({ title, published_year, data })
-
-    // NOTE: 6초 -> 1.8초
-    // dbCreateBookWithConcurrency({ title, published_year, data })
 }
