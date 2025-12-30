d6329dd256e41cbcb18168ca899478b37208cbd4
Tynahan <nusilite@gmail.com>
2025-12-30 13:32:38 +0900

    feat(schema): add book, topic models

---
+++ src/db/bookDb.ts
@@ -0,0 +1,6 @@
+import prismaClient from "./prismaClient.js"
+
+export const dbCheckIfBookExists = async (bookTitle: string): boolean => {
+    // const result = prismaClient.
+    return false // TODO: MUST REPLACE
+}
