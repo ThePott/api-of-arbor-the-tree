79f6751353f7eec17ef874067517c1c727d2301b
Tynahan <nusilite@gmail.com>
2025-12-30 12:21:09 +0900

    feat(script): connect to debugger by default

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { bookArrayDummy, type Book } from "../dummy/bookDummy.js"
+import { extractAccessToken } from "../utils/extractAccessToken.js"
 
 const bookRouter = Router()
 
 type BookActivity = "active" | "inactive" | "total"
 
 bookRouter.get("/", async (req, res) => {
+    extractAccessToken(req.headers) // TODO: 지금은 access token을 검증하지 않음
     const activity = String(req.query.activity) as BookActivity
 
     // NOTE: THIS IS TEMPORARY DUMMY DATA
@@ -25,6 +27,28 @@ bookRouter.get("/", async (req, res) => {
         }
     })
     res.status(200).json(filteredBookArray)
+})
+
+bookRouter.get("/detail", async (req, res) => {
+    // extractAccessToken(req.headers) // TODO: 지금은 access token을 검증하지 않음
+
+    // NOTE: 이걸로 실제로 검색을 해야 함
+    const query = String(req.query.query)
+
+    // NOTE: 지금은 더미로 보냄
+    const result: string[] = [
+        "p.26 확인 1 ____ [I] 나머지정리 A단계",
+        "p.26 확인 2 ____ [I] 나머지정리 A단계",
+        "p.26 확인 3 ____ [I] 나머지정리 A단계",
+        "p.26 확인 4 ____ [I] 나머지정리 A단계",
+        "p.26 확인 5 ____ [I] 나머지정리 A단계",
+        "p.27 확인 6 ____ [I] 나머지정리 A단계",
+        "p.27 확인 7 ____ [I] 나머지정리 A단계",
+        "p.27 확인 8 ____ [I] 나머지정리 A단계",
+        "p.27 확인 9 ____ [I] 나머지정리 A단계",
+        "p.27 확인 10 ____ [I] 나머지정리 A단계",
+    ]
+    res.status(200).json(result)
 })
 
 export default bookRouter
