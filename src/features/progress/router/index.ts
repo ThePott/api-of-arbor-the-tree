000d47f04afdb083ec558c30a4963d8ae885aad7
Tynahan <nusilite@gmail.com>
2026-01-23 21:17:18 +0900

    fix(progress): change route for assigned

--- src/features/progress/router/index.ts
+++ src/features/progress/router/index.ts
@@ -11,8 +11,16 @@ import { ApiError } from "@/src/errors/appError/AppError.js"
 
 const progressRouter = Router()
 
+// NOTE: auto complete에 사용할 실라버스 받아와야
+progressRouter.get("/syllabus", async (req, res) => {
+    const user_id = extractUserId(req.headers)
+    const result = await dbProgressFindManySyllabus(user_id)
+    const serializable = makeSerializable(result)
+    res.status(200).json(serializable)
+})
+
 // NOTE: 과정 추가할 때
-progressRouter.post("/syllabus", async (req, res) => {
+progressRouter.post("/syllabus/assigned", async (req, res) => {
     const body = req.body // NOTE: book_id, classroom_id, student_id
     const user_id = extractUserId(req.headers)
 
@@ -23,14 +31,6 @@ progressRouter.post("/syllabus", async (req, res) => {
     const result = await dbProgressCreateSyllabus({ ...body, user_id })
     const serializable = makeSerializable(result)
     console.log({ serializable })
-    res.status(200).json(serializable)
-})
-
-// NOTE: auto complete에 사용할 실라버스 받아와야
-progressRouter.get("/syllabus", async (req, res) => {
-    const user_id = extractUserId(req.headers)
-    const result = await dbProgressFindManySyllabus(user_id)
-    const serializable = makeSerializable(result)
     res.status(200).json(serializable)
 })
 
@@ -47,7 +47,7 @@ progressRouter.get("/syllabus/assigned", async (req, res) => {
     res.status(200).json(serializable)
 })
 
-progressRouter.delete("/syllabus/:syllabus_id", async (req, res) => {
+progressRouter.delete("/syllabus/assigned/:syllabus_id", async (req, res) => {
     const syllabus_id = BigInt(req.params.syllabus_id)
     const classroom_id = req.query.classroom_id ? BigInt(String(req.query.classroom_id)) : null
     const student_id = req.query.student_id ? BigInt(String(req.query.student_id)) : null
