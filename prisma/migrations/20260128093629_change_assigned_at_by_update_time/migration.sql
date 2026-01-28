565bca4704df63a0894e8389c3688e586e5d19cf
Tynahan <nusilite@gmail.com>
2026-01-28 18:37:30 +0900

    fix(schema): change assigned_at to updatedAt

---
+++ prisma/migrations/20260128093629_change_assigned_at_by_update_time/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "assigned_session_classroom" ALTER COLUMN "assigned_at" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "assigned_session_student" ALTER COLUMN "assigned_at" DROP DEFAULT;
