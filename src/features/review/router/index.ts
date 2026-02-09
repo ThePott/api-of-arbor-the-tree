32d8636841ce2fbeee48bb56efd0c9760b436d75
Tynahan <nusilite@gmail.com>
2026-02-09 14:29:40 +0900

    chore(review): remove dead attribute from type

--- src/features/review/router/index.ts
+++ src/features/review/router/index.ts
@@ -59,7 +59,6 @@ type QuestionIdToInfoFromClient = Record<
     {
         status: review_check_status | null // NOTE: use to delete if null
         review_check_id: string | null
-        question_id: string
         assigned_session_student_id: string // NOTE: 오답 체크는 부여된 묶음에서만 가능함
     }
 >
