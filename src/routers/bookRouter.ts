ea03c4bd78d11f687dc7ffd2bd772c8fcd1cec35
Tynahan <nusilite@gmail.com>
2026-03-09 16:33:04 +0900

    feat(auth): add checking permission helper function

--- src/routers/bookRouter.ts
+++ src/routers/bookRouter.ts
@@ -4,7 +4,7 @@ import { validateBody } from "../utils/validateBody.js"
 import { makeSerializable } from "../utils/makeSerializable.js"
 import { extractPermission } from "../utils/decodeAccessToken.js"
 import { convertToBigIntOrThrow } from "../utils/convertToBigInt.js"
-import { makeAllowedRoleArray } from "../utils/make-allowed-role-array.js"
+import { validatePermission } from "../utils/make-allowed-role-array.js"
 import { ApiError } from "../errors/appError/AppError.js"
 import type { BookWritePayload } from "../interfaces/interfaces.js"
 
@@ -15,22 +15,23 @@ const bookRouter = Router()
 bookRouter.get("/", async (req, res) => {
     const { user_id: _, role, hagwon_id } = extractPermission(req.headers)
     if (!role) throw ApiError.Forbidden("접근 권한이 없어요")
-    const allowedRoleArray = makeAllowedRoleArray("HELPER")
-    if (!allowedRoleArray.includes(role)) throw ApiError.Forbidden("접근 권한이 없어요")
+    validatePermission({ minimumRole: "HELPER", currentRole: role })
     const result = await dbFindManyBook(hagwon_id)
     const serializable = makeSerializable(result)
     res.status(200).json(serializable)
 })
 
 bookRouter.delete("/:bookId", async (req, res) => {
-    const { user_id, hagwon_id } = extractPermission(req.headers) // TODO: 지금은 access token을 검증하지 않음
+    const { user_id, role, hagwon_id } = extractPermission(req.headers)
+    validatePermission({ minimumRole: "PRINCIPAL", currentRole: role })
+
     const book_id = convertToBigIntOrThrow(req.params.bookId)
     await dbDeleteBook({ user_id, hagwon_id, book_id })
     res.status(204).send()
 })
 
 bookRouter.post("/write", async (req, res) => {
-    const { user_id, hagwon_id } = extractPermission(req.headers) // TODO: 지금은 access token을 검증하지 않음
+    const { user_id, hagwon_id } = extractPermission(req.headers)
 
     const { title, published_year, data } = req.body
     validateBody({ title, published_year, data })
