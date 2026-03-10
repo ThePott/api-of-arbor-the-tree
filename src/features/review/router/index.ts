cb67c51a1dae8f8ee7214bf497317a839d7ecfca
Tynahan <nusilite@gmail.com>
2026-03-10 14:38:22 +0900

    fix(check): group questions by topic even for assignment review

--- src/features/review/router/index.ts
+++ src/features/review/router/index.ts
@@ -74,9 +74,11 @@ const condenseAssignmentWithBooksArray = (extended: ReturnType<typeof addAttempt
     const condensed = extended.map((assignment) => {
         const books = assignment?.books?.map((book) => {
             const { topics: _, ...rest } = book
-            const questions = book.topics?.flatMap((topic) => topic.steps.flatMap(({ questions }) => questions))
-            // .map((question) => ({ ...question, assignment_status: assignment.status }))
-            return { ...rest, questions }
+            const topics = book.topics?.map(({ steps, ...rest }) => {
+                const questions = steps.flatMap(({ questions }) => questions)
+                return { ...rest, questions }
+            })
+            return { ...rest, topics }
         })
         return { ...assignment, books }
     })
