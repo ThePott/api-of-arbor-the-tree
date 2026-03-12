90e134efbbe8712145a1e6f8e956c12074d9f2b3
Tynahan <nusilite@gmail.com>
2026-03-12 15:29:52 +0900

    feat(check): show even reviewed question when fetch assignment questions

--- src/shared/queries/filter-by-assignment.ts
+++ src/shared/queries/filter-by-assignment.ts
@@ -17,7 +17,7 @@ function filterByAssignment(props: { forWhat: "topic"; assignment_id: bigint }):
 function filterByAssignment(props: { forWhat: "book"; assignment_id: bigint }): bookWhereInput
 function filterByAssignment({ forWhat, assignment_id }: FilterByAssignment) {
     const questionAttemptWhereInput: question_attemptWhereInput = {
-        child_attempt: null, // TODO: 이게 필요한가? 이 오답과제의 모든 체크를 가져온다고 하면 없어도 되지 않나???
+        // child_attempt: null, // TODO: 이게 필요한가? 이 오답과제의 모든 체크를 가져온다고 하면 없어도 되지 않나???
         review_assignment_id: assignment_id,
     }
     if (forWhat === "questionAttempt") return questionAttemptWhereInput
