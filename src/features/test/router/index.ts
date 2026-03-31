2f40d692194f0347e97c9907656e3787c340d35f
Tynahan <nusilite@gmail.com>
2026-03-31 18:42:40 +0900

    feat(test): add route for react-pdf

--- src/features/test/router/index.ts
+++ src/features/test/router/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import makeAssignmentPdf from "../../assignment/pdf/index.js"
 import testBook from "./test-book/index.js"
+import { makeSerializable } from "@/src/utils/makeSerializable.js"
 
 const testRouter = Router()
 
-testRouter.get("/pdf", async (req, res) => {
+testRouter.get("/pdf/typst", async (req, res) => {
     const multiplier = Number(req.query.multiplier)
     console.time(`>>>> pdf requsted with multiplier ${multiplier}`)
     const pdf = makeAssignmentPdf({
@@ -17,6 +18,18 @@ testRouter.get("/pdf", async (req, res) => {
     res.contentType("application/pdf")
     res.status(200).send(pdf)
     // res.status(200).send()
+})
+
+testRouter.get("/pdf/client", async (req, res) => {
+    const multiplier = Number(req.query.multiplier)
+    const pdfInfo = {
+        id: BigInt(1),
+        studentName: "홍길동",
+        assigned_at: new Date(),
+        bookForPdfArray: Array(multiplier).fill(testBook),
+    }
+    const serialized = makeSerializable(pdfInfo)
+    res.status(200).json(serialized)
 })
 
 export default testRouter
